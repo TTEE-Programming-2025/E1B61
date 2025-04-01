6197f29c7508ad2ffb69f18c30feca90c0df4b65
張介誠 <jasper20060710@gmail.com>
2025-04-01 20:25:58 +0800

    寫了一個新程式

---
+++ E1B61.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+	int password,key=2025;//宣告變數 
+	char Char;//宣告字元 
+	printf("            ##     ###        ###      ##\n");
+	printf("           ###      ##       ##       ###\n");
+	printf("  ####      ##      ##      ##         ##\n");
+	printf(" ##  ##     ##      #####   #####      ##\n");
+	printf(" ######     ##      ##  ##  ##  ##     ##\n");
+	printf(" ##         ##      ##  ##  ##  ##     ##\n");
+	printf("  #####   ######   ######    ####    ######\n");
+	printf("---------------------------------------------\n");
+	printf("            ##     ###        ###      ##\n");
+	printf("           ###      ##       ##       ###\n");
+	printf("  ####      ##      ##      ##         ##\n");
+	printf(" ##  ##     ##      #####   #####      ##\n");
+	printf(" ######     ##      ##  ##  ##  ##     ##\n");
+	printf(" ##         ##      ##  ##  ##  ##     ##\n");
+	printf("  #####   ######   ######    ####    ######\n");
+	printf("---------------------------------------------\n");
+	printf("            ##     ###        ###      ##\n");
+	printf("           ###      ##       ##       ###\n");
+	printf("  ####      ##      ##      ##         ##\n");
+	printf(" ##  ##     ##      #####   #####      ##\n");
+	printf(" ######     ##      ##  ##  ##  ##     ##\n");
+	printf(" ##         ##      ##  ##  ##  ##     ##\n");
+	printf("  #####   ######   ######    ####    ######\n"); 
+	system("pause");
+	printf("請輸入4位數字密碼:");
+	scanf("%d",&password);//輸入密碼 
+	if(password !=key){
+		printf("密碼錯誤，程式結束\n");
+		printf("%c",'\a');
+		return 0;//密碼錯誤，程式結束
+	}
+	//密碼正確，顯示歡迎訊息 
+	printf("歡迎，密碼正確\n");
+	system("cls");//清除螢幕 
+	printf("'A'...'Z':Uppercase\n");
+	printf("'a'...'z':Lowercase\n");
+	printf("'0'...'9':Digit\n");
+	printf("Othercase:E1B61張介誠\n");
+	printf("請輸入1個字元:");//輸入1字元 
+	scanf(" %c",&Char);
+	//判斷輸入的字元 
+	if(Char>='A'&&Char<='Z'){
+		printf("Uppercase\n");
+	} else if(Char>='a'&&Char<='z'){
+		printf("Lowercase\n");
+	} else if(Char>='0'&&Char<='9'){
+		printf("Digit\n");
+	} else {
+		printf("E1B61張介誠\n");
+	}
+	system("pause");
+	return 0;
+}//
