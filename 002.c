8edb5d2b9ebc11204f7003699bc69c932bf3da79
jasper <jasper20060710@gmail.com>
2025-04-26 20:00:05 +0800

    新增註解與心得

--- 002.c
+++ 002.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
-#include <ctype.h>
+#include <conio.h>// 使用getch()函數
+#include <ctype.h>// 處理大小寫字母
 int main(void) 
 {
     int password,n=0;//宣告變數
@@ -28,33 +28,34 @@ int main(void)
 	printf(" ##  ##     ##      #####   #####      ##\n");
 	printf(" ######     ##      ##  ##  ##  ##     ##\n");
 	printf(" ##         ##      ##  ##  ##  ##     ##\n");
-	printf("  #####   ######   ######    ####    ######\n"); 
+	printf("  #####   ######   ######    ####    ######\n");//個人風格畫面 
 	system("pause");
+	//輸入密碼，最多可以嘗試三次
     do{
         printf("輸入密碼:");
         scanf("%d",&password);
-        n++;
+        n++;//增加錯誤次數
         if(password==2025)
-        break;
+        break;//如果密碼正確，跳出循環
         if(n<2){
-        	printf("還剩%d次\n",(3-n));
+        	printf("還剩%d次\n",(3-n));//顯示剩餘機會
 		}else{
 			printf("錯誤三次不能輸入了");
-            return 0;
+            return 0;//超過三次錯誤，退出程式
 		}
     }
     while(n<3);
-    system("cls");/*清除當前畫面*/
+    system("cls");//清除當前畫面
     do{
 	    printf("( a.直角三角形 )\n"); 
 	    printf("( b.顯示乘法表 )\n"); 
 	    printf("( c.結束 )\n"); 
-	    printf("請輸入字元\n"); 
+	    printf("請輸入字元:\n"); 
         scanf(" %c",&ch);
         switch (ch) 
 	    {
             case 'a': 
-	        {
+	        { //選擇 'a'，顯示直角三角形
                 int i, j, h;
                 printf("請輸入三角形的高度: ");
                 scanf("%d", &h);
@@ -66,11 +67,11 @@ int main(void)
                     printf("\n");
                 }
                 printf("按任意鍵返回主選單...\n");
-                getch();
+                getch();//按任意鍵
                 break;
             }
             case 'b': 
-	        {
+	        {  //選擇 'b'，顯示乘法表 
                 int i, j;
                 for (i = 1; i <= 9; i++)
 		        {
@@ -80,31 +81,40 @@ int main(void)
                     printf("\n");
                 }
                 printf("按任意鍵返回主選單...\n");
-                getch();
+                getch();//按任意鍵
                 break;
             }
-            case 'c':{
+            case 'c':{// 選擇 'c'，詢問是否繼續
 				char confirm;
                 do {
                     printf("\nContinue? (y/n)：");
                     scanf(" %c", &confirm);
-                    confirm = tolower(confirm);
+                    confirm = tolower(confirm);//將輸入轉換為小寫字母
                     if (confirm == 'y') {
-                        break;
+                        break;//重新回到主選單
                     } else if (confirm == 'n') {
                         printf("\n程式結束，感謝使用！\n");
                         return 0;
                     } else {
-                        printf("無效輸入，請輸入 y 或 n。\n");
+                        printf("無效輸入，請輸入 y 或 n。\n");//顯示錯誤訊息
                     }
-                } while (1);
+                } while (1);//不斷重複直到得到有效的輸入
                 break;
             }
             default:
-                printf("無效選項，請重新輸入。\n");
+                printf("無效選項，請重新輸入。\n");//顯示錯誤訊息
                 system("pause");
         } 
     }
-        while(1);	
+        while(1);//主選單循環直到退出程式	
     return 0;
 }
+
+
+
+
+//心得:這次的作業練習了多層 switch 與do-while 控制結構， 
+//在編寫程式的過程中出了各種問題:
+//比如scanf忘記前面加空格、沒加 #include <conio.h>導致編譯錯誤，
+//而且還在case'c'的地方卡了很久，
+//但也正因如此才讓我體會到我的不足 
