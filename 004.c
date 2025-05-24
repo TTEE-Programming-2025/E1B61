8c84f23bf6e971a220d433d310c7eb6874abbc84
jasper <jasper20060710@gmail.com>
2025-05-24 16:15:23 +0800

    新增主選單和選擇功能

--- 004.c
+++ 004.c
@@ -4,6 +4,7 @@
 int main(void) 
 {
     int password,n=0;
+    char choice;
     printf("            ##     ###        ###      ##\n");
 	printf("           ###      ##       ##       ###\n");
 	printf("  ####      ##      ##      ##         ##\n");
@@ -42,8 +43,47 @@ int main(void)
             return 0;
 		}
     }while(1);
+    do {
+        system("cls");
+        printf(" ------------[Grade System]----------\n");
+        printf(" | a. Enter student grades           |\n");
+        printf(" | b. Display student grades         |\n");
+        printf(" | c. Search for student grades      |\n");
+        printf(" | d. Grade ranking                  |\n");
+        printf(" | e. Exit system                    |\n");
+        printf(" ------------------------------------\n");
+        printf("請選擇功能(a~e): ");
+        scanf(" %c", &choice);
+        getchar();
+        switch (choice) {
+            case 'a':
+                printf("你選擇了：輸入成績\n");
+                system("pause");
+                break;
+            case 'b':
+                printf("你選擇了：顯示成績\n");
+                system("pause");
+                break;
+            case 'c':
+                printf("你選擇了：搜尋成績\n");
+                system("pause");
+                break;
+            case 'd':
+                printf("你選擇了：成績排名\n");
+                system("pause");
+                break;
+            case 'e':
+                printf("系統結束，掰掰！\n");
+                break;
+            default:
+                printf("無效選項，請重新輸入。\n");
+                system("pause");
+                break;
+        }
+    } while (choice != 'e');
     return 0;
-} 
+}
+
 	
 	
 	
